d0f7dbef23aa2082b7c2d503130e65c65d9ff11c
Deepak Guptha <vzan2012@gmail.com>
2023-06-22 22:00:46 +0530

    feat: updated redux - my cart button

--- src/components/Cart/CartButton.jsx
+++ src/components/Cart/CartButton.jsx
@@ -2,14 +2,14 @@ import { useDispatch } from "react-redux";
 import classes from "./CartButton.module.css";
 import { uiActions } from "../../store/ui-slice";
 
-// TODO: Implement the Dispatch Actions from the UI Slice
 const CartButton = () => {
-  const cartHandler = () => {
-    // const useDispatch
+  const dispatch = useDispatch();
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
   };
 
   return (
-    <button className={classes.button} onClick={cartHandler}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
--- src/store/cart-slice.jsx
+++ src/store/cart-slice.jsx
@@ -3,12 +3,29 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    cartIsVisible: false,
+    items: [],
+    totalQuantity: 0,
+    totalAmount: 0,
   },
   reducers: {
-    toggle(state) {
-      state.cartIsVisible = !state.cartIsVisible;
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
     },
+    removeItemFromCart(state, action) {},
   },
 });
 
