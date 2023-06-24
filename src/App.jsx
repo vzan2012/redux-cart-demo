6e9c0fdf9a44ff8f8f8cc6216e123a6adb304ae5
Deepak Guptha <vzan2012@gmail.com>
2023-06-24 18:15:21 +0530

    feat: fetch data from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 const App = () => {
+  const PRODUCTS_URL = import.meta.env.VITE_FIREBASE_URL;
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(`${PRODUCTS_URL}/cart.json`, {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
   return (
     <Layout>
       {showCart && <Cart />}
