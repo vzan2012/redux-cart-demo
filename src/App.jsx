4c3c20384be883842648d72d64868796414e5d4e
Deepak Guptha <vzan2012@gmail.com>
2023-06-25 20:51:37 +0530

    feat: updated action creators - thunk

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,13 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
+
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true;
 
 const App = () => {
-  const PRODUCTS_URL = import.meta.env.VITE_FIREBASE_URL;
-
   const dispatch = useDispatch();
 
   const showCart = useSelector((state) => state.ui.cartIsVisible);
@@ -18,48 +17,16 @@ const App = () => {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending ...",
-          message: "Sending cart data",
-        })
-      );
-      const response = await fetch(`${PRODUCTS_URL}/cart.json`, {
-        method: "PUT",
-        body: JSON.stringify(cart),
-      });
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success !!!",
-          message: "Sent cart data successfully !!!",
-        })
-      );
-    };
-
-    // const responseData = await response.json();
+    // const sendCartData = async () => {};
+    // // const responseData = await response.json();
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((error) =>
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error !!!",
-          message: "Sending cart data failed !!!",
-        })
-      )
-    );
-  }, [cart, dispatch, PRODUCTS_URL]);
+    dispatch(sendCartData(cart));
+    // sendCartData().catch((error) => { });
+  }, [cart, dispatch]);
   return (
     <>
       {notification && (
--- src/store/cart-slice.jsx
+++ src/store/cart-slice.jsx
@@ -1,4 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
+
+const PRODUCTS_URL = import.meta.env.VITE_FIREBASE_URL;
 
 const cartSlice = createSlice({
   name: "cart",
@@ -40,6 +43,50 @@ const cartSlice = createSlice({
     },
   },
 });
+
+// Action Creators
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending ...",
+        message: "Sending cart data",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(`${PRODUCTS_URL}/cart.json`, {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success !!!",
+          message: "Sent cart data successfully !!!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error !!!",
+          message: "Sending cart data failed !!!",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
